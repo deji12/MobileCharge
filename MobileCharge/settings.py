0e6c9b8030bb3ca596af6ade7efad7875e9a8d5e
deji12 <adesolaayodeji53@gmail.com>
2024-10-07 15:17:50 +0100

    changed db

--- MobileCharge/settings.py
+++ MobileCharge/settings.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 import os
 from corsheaders.defaults import default_headers
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,6 +163,7 @@ DATABASES = {
     }
 }
 
+DATABASES["default"] = dj_database_url.parse(config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
