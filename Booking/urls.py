81bda370d2c8dcd6a5d7715ad0e3bf29bb039f94
deji12 <adesolaayodeji53@gmail.com>
2024-10-12 18:56:19 +0100

    fixed error in get_booking endpoint

--- Booking/urls.py
+++ Booking/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('create-booking/', views.create_booking, name='create-booking'),
     path('bookings/<str:booking_status>/', views.get_bookings, name='bookings'),
-    path('<int:booking_id>/', views.get_booking, name='get_booking'),
+    path('<str:booking_id>/', views.get_booking, name='get_booking'),
     path('update-booking-status/<str:invoice_id>/', views.update_booking_status),
 ]
